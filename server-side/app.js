e141deba2ea4025f5a3909293d485803860d1500
Harjot Singh <150969623+harjotsk03@users.noreply.github.com>
2024-10-06 08:58:24 -0700

    allow anywhere

--- server-side/app.js
+++ server-side/app.js
@@ -13,21 +13,13 @@ app.use(cookieParser());
 
 app.use(express.json());
 
-const allowedOrigins = ["http://localhost:3001", "http://localhost:3000", "https://bin-there-dumped-that.vercel.app", "https://bin-there-dumped-that-git-master-caleb05wgmailcoms-projects.vercel.app", "https://bin-there-dumped-that-gdjgzw5u7-caleb05wgmailcoms-projects.vercel.app",];
-
 app.use(
   cors({
-    origin: allowedOrigins,
-    credentials: true,
+    origin: "*", // Allow all origins
+    credentials: true, // This is necessary for cookies to be sent
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://bin-there-dumped-that.vercel.app"/);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  next();
-});
 
 mongoose
   .connect(process.env.MONGO_URI, {})
