10f0f9578f671e59d68d083d565e75800c944222
Harjot Singh <150969623+harjotsk03@users.noreply.github.com>
2024-10-06 08:29:17 -0700

    add vercel link

--- server-side/app.js
+++ server-side/app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 app.use(express.json());
 
-const allowedOrigins = ["http://localhost:3001", "http://localhost:3000"];
+const allowedOrigins = ["http://localhost:3001", "http://localhost:3000", "bin-there-dumped-that.vercel.app"];
 
 app.use(
   cors({
